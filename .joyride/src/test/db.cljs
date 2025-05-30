d1ac571c676198acce6969d7028dd6ecd42b04b8
PEZ <pez@pezius.com>
2025-05-31 01:52:26 +0200

    WIP: Add Seatbelt

---
+++ .joyride/src/test/db.cljs
@@ -0,0 +1,10 @@
+(ns test.db
+  (:require [cljs.test]))
+
+(def default-db {:running nil
+                 :pass 0
+                 :fail 0
+                 :error 0})
+
+(def !state (atom default-db))
+
